756156fe42729191dbb8fefa6d2a9766567be425
HunterProgram22 <JustinKudela@gmail.com>
2018-05-16 16:22:23 -0400

    Continue updating templates.

--- Golf/urls.py
+++ Golf/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Home, Manage, Rounds9, Rounds18
+from .views import Home, Manage, Rounds9, Rounds18, Handicap
 
 urlpatterns = [
     path('', Home.as_view(), name='Golf_Home'),
     path('Golf/Manage/', Manage.as_view(), name='Golf_Manage'),
     path('Golf/Rounds9/', Rounds9.as_view(), name='Golf_Rounds9'),
     path('Golf/Rounds18/', Rounds18.as_view(), name='Golf_Rounds18'),
+    path('Golf/Handicap/', Handicap.as_view(), name='Golf_Handicap'),
 
 ]
--- Golf/views.py
+++ Golf/views.py
@@ -8,23 +8,30 @@ class Home(View):
     def get(self, request):
         latest_round_list = Round.objects.order_by('-date')[:5]
         context = {'latest_round_list': latest_round_list}
-        return render(request, 'Golf/GolfBase.html', context)
+        return render(request, 'Golf/Home.html', context)
 
 
 class Manage(View):
     def get(self, request):
+        context = None
         return render(request, 'Golf/Manage.html', context)
 
 
 class Rounds9(View):
     def get(self, request):
         #round_stats9 = Round.objects.filter(holesplayed=9).order_by('-date')
-        #context = {'round_stats9': round_stats9}
+        context = None
         return render(request, 'Golf/Rounds9.html', context)
 
 
 class Rounds18(View):
     def get(self, request):
         #round_stats = Round.objects.filter(holesplayed=18).order_by('-date')
-        #context = {'round_stats': round_stats}
-        return render(request, 'Golf/Rounds.html', context)
+        context = None
+        return render(request, 'Golf/Rounds18.html', context)
+
+
+class Handicap(View):
+    def get(self, request):
+        context = None
+        return render(request, 'Golf/Handicap.html', context)
